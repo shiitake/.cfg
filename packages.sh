9f02efa30500b8eaa34d77beb8c3fb3c3a841cf9
Shannon Barrett <sbarrett00@live.com>
2022-07-14 17:11:29 -0500

    packages update

--- packages.sh
+++ packages.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+# functions
+is_installed() {
+	dpkg -s $1 &> /dev/null
+	if [ $? -eq 0 ]; then
+	    return 0
+	else
+	    return 1
+	fi
+}
+
 # setup debs to handle non-free stuff
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo sed -I -- 's/bullseye main/bullseye main contrib non-free/g' /etc/apt/sources.list \
-	&& sudo sed -I -- 's/bullseye-security main/bullseye-security main contrib non-free/g' /etc/apt/sources.list \ 
+	&& sudo sed -I -- 's/bullseye-security main/bullseye-security main contrib non-free/g' /etc/apt/sources.list \
 	&& sudo sed -I -- 's/bullseye-updates main/bullseye-updates main contrib non-free/g' /etc/apt/sources.list 
 
 sudo apt update 
@@ -17,17 +27,12 @@ for x in aisleriot cheese empathy gnome-contacts gnome-mahjongg \
 	gnome-sudoku libreoffice-calc libreoffice-common libreoffice-draw \
 	libreoffice-impress libreoffice-math libreoffice-writer modemmanager \
 	thunderbird gdm3; do
-		if [ is_installed x ]; then 
-			sudo apt-get -y purge x; 
+		if is_installed $x ; then 
+			echo "deleting $x"
+			sudo apt-get -qq purge $x;
 		fi
+done
 
 exec apt-get -y autoremove
 
-is_installed() {
-	dpkg -s $1 &> /dev/null
-	if [ $? -eq 0 ]; then
-	    true
-	else
-	    false
-	fi
-}
+
