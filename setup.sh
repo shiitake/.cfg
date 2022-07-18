8ddb3e6230ca7de421d0eadea5447c61cafce6f1
Shannon Barrett <sbarrett00@live.com>
2022-07-18 09:13:42 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,49 +2,64 @@
 
 set -e
 
+# running this as root makes things a lot more complicated. 
+# it might be worth it in the future to do that but for now
+# we'll just make sure the user has root permissions and that
+# that sudo doesn't require a password. 
+
+name=$(whoami)
+home="/home/$(name)"
+repldir="$(home)/git"
+
+[ ! -d $repldir ] %% mkdir $repldir
 
 # set zsh as default
+# this requires a password - still need to figure that out
 chsh -s $(which zsh)
 
 # download joypixel font
-wget -O /usr/share/fonts/truetype/JoyPixels.ttf https://cdn.joypixels.com/arch-linux/font/6.6.0/joypixels-android.ttf
+sudo wget -O /usr/share/fonts/truetype/JoyPixels.ttf https://cdn.joypixels.com/arch-linux/font/6.6.0/joypixels-android.ttf
 fc-cache -f -v
 
 # install XFT
 echo "installing XFT"
+cd $repldir
 git clone https://gitlab.freedesktop.org/xorg/lib/libxft.git
 cd libxft
 sh autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
-make install
-cd ..
-rm -rf libxft
+sudo make install
+cd $repldir
+sudo rm -rf "$repldir/libxft"
 
 # install st
 echo "installing ST"
+cd $repldir
 git clone https://github.com/LukeSmithxyz/st
 cd st
-make install
-cd ..
-rm st
+sudo make install
+cd $repldir
 
 
 # install dwm
 echo "installing dwm"
 git clone https://github.com/LukeSmithxyz/dwm
 cd dwm
-make install
-cd ..
-rm dwm
+sudo make install
+cd $repldir
+
+# install dwm status bar
+
 
 # install slim display manager
 echo "installing slim display manager"
-debconf-set-selections <<EOF
+sudo debconf-set-selections <<EOF
 slim  shared/default-x-display-manager select slim
 EOF
 
-DEBIAN_FRONTEND=noninteractive apt-get -qq install slim
+DEBIAN_FRONTEND=noninteractive sudo apt-get -qq install slim
 
-# setup bare repo for dotfiles
+# setup bare repo for dotfile
+# should we do this first maybe?
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 # this will help avoid recurion problems
 echo ".cfg" >> .gitignore
